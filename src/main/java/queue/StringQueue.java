230743ebdded015b19cd07bd40b086adc9541717
Michael189-debug <74509946+Michael189-debug@users.noreply.github.com>
2021-01-17 21:35:46 +0100

    commit from the end of lesson

---
+++ src/main/java/queue/StringQueue.java
@@ -0,0 +1,26 @@
+package queue;
+
+import list.StringLinkedList;
+
+    //TODO samodzielnie napisz testy
+    public class StringQueue {
+        StringLinkedList list = new StringLinkedList();
+
+        public void insert(String item){
+            list.add(item);
+        }
+
+        public String remove(){
+            String item = list.get(0);
+            list.remove(0);
+            return item;
+        }
+
+        public boolean isEmpty(){
+            return list.isEmpty();
+        }
+
+        public int size(){
+            return list.size();
+        }
+    }
